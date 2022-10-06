83592626a5ebb0cd802bfd9ad88561d8dec81ec9
anuch <anuchem2004@gmail.com>
2022-10-06 17:51:59 -0400

    Dbt files first commit 06 Oct 2022

---
+++ pythonProject_dbt_dev/dbt2_airflow_poc/models/example/hrst/wt_email_d.sql
@@ -0,0 +1,17 @@
+
+{{ config(materialized='table', alias='wt_email_d') }}
+
+with wt_email_d as (
+
+    SELECT
+            email_sk,
+            email_auth,
+           email_valid
+
+
+    FROM {{source('hrst','wt_email_d')}}
+    )
+
+SELECT * FROM wt_email_d
+
+
